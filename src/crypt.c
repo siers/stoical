3b548dfb222a842d77195631db4caf361c521912
siers <wimuan@gmail.com>
2012-07-08 15:12:02 +0300

    Find the ".text".

--- src/crypt.c
+++ src/crypt.c
@@ -5,6 +5,8 @@ static struct executable {
     Elf *elf;
     Elf_Kind kind;
     GElf_Ehdr ehdr;
+
+    GElf_Shdr text;
 } exec;
 
 static void
@@ -18,6 +20,32 @@ do_checks()
             "input file must be an executable");
 }
 
+static void
+print_info()
+{
+    log("The entry point is at 0x%.8x.", exec.ehdr.e_entry);
+    log(".text.size = 0x%xb", exec.text.sh_size);
+}
+
+static void
+find_text()
+{
+    Elf_Scn *scn = 0;
+    char* sh_name;
+
+    while ((scn = elf_nextscn(exec.elf, scn)) != 0)
+    {
+        gelf_getshdr(scn, &exec.text);
+        sh_name = elf_strptr(exec.elf, exec.ehdr.e_shstrndx, exec.text.sh_name);
+
+        if (strcmp(sh_name, ".text") == 0) {
+            return log(".text found");
+        }
+    }
+
+    assert_fatal(0, "Couldn't find .text section.");
+}
+
 /* Doesn't really crypt, just a name for the entry point.  */
 int
 crypt()
@@ -25,6 +53,8 @@ crypt()
     assert_fatal((exec.elf = elf_begin(input, ELF_C_READ, NULL)) != NULL,
             "elf_begin failed: %s", elf_errmsg(-1));
     do_checks();
+    find_text();
+    print_info();
     elf_end(exec.elf);
 
     return 0;
